d5766c2b72ae2930383af00d6a233f483a260677
bhaecker <44894840+bhaecker@users.noreply.github.com>
2020-01-21 00:32:19 +0100

    Update ROC_curve.py

--- ROC_curve.py
+++ ROC_curve.py
@@ -23,7 +23,7 @@ fpr_keras, tpr_keras, thresholds_keras = roc_curve(y, y_pred)
 auc_keras = auc(fpr_keras, tpr_keras)
 plt.figure(1)
 plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+plt.plot(fpr_keras, tpr_keras, label='Classifier (area = {:.3f})'.format(auc_keras))
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 
