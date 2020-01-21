4e8b1d2e5dcd3a0fc323aa7043b783d29995e208
bhaecker <44894840+bhaecker@users.noreply.github.com>
2020-01-21 01:33:21 +0100

    Update findKermit.py

--- findKermit.py
+++ findKermit.py
@@ -4,8 +4,9 @@ from keras.models import load_model
 import math
 
 #the function to test if Kermit is present in a video sequence:
-#start and end in seconds for the boundaries of the sequence, video is the name of the video 
+#start and end in seconds for the boundaries of the sequence, video is the name of the video
 #model_images.h5 and model_audio.h5 have to be in the same path as this script
+#model_images.h5 is categorical, model_audio.h5 binary
 
 def ishethere(start,end,video):
     if end <= start:
@@ -13,23 +14,21 @@ def ishethere(start,end,video):
 
     #Images
     model_images = load_model('model_images.h5')
-
     X = np.load('images_'+video+'.npy')[start:end] #multiply start and end by the FPS, e.g 2FPS -> [start*2,end*2]
     #make predictions
     y_pred = model_images.predict(X)
     max_value_images = np.max(y_pred.T, axis=1)[1]
-    print(max_value_images)
     # data type for output
     max_value_images_str=str(max_value_images.astype(np.float))
 
     #Audio
-    X = np.load('audio_'+video+'.npy')[math.floor(start*86.12):math.ceil(end*86.12)]
+    X = np.load('testchunks.npy')[math.floor(start*86.12):math.ceil(end*86.12)]#!!!check how to obtain time interval
     X = np.expand_dims(X, axis=2)
 
     model_audio = load_model('model_audio.h5')
     # make predictions
     y_pred = model_audio.predict(X)
-    max_value_audio = np.max(y_pred.T, axis=1)[1]
+    max_value_audio = 1 - np.min(y_pred) #0 Kermit present, 1 he is not
     #data type for output
     max_value_audio_str = str(max_value_audio.astype(np.float))
 
@@ -48,4 +47,4 @@ def ishethere(start,end,video):
         return('Kermit is not present with an accuracy of '+fail_acc_str)
 
 
-print(ishethere(0,5,'Muppets-02-01-01'))
+print(ishethere(0,5,'Muppets-02-04-04'))
